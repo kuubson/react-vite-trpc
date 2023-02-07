9fe3ad4ba6f37f0d200ec6f417956f70f457321c
kuubson <jxtoxic24@gmail.com>
2023-02-07 22:41:22 +0100

    simplify initializing trpc due to the fixed absolute paths

--- apps/server/trpc/trpc.ts
+++ apps/server/trpc/trpc.ts
@@ -1,29 +1,11 @@
 import { initTRPC } from "@trpc/server";
 
-const isWeb = typeof document !== "undefined";
-
 import type { Context } from "./api/router";
 
-// NOTE: initializing tRPC in this manner prevents the error "Uncaught Error: You're trying to use @trpc/server in a non-server environment. This is not supported by default.
+const t = initTRPC.context<Context>().create();
 
-const trpcServer = () => {
-  if (!isWeb) {
-    const t = initTRPC.context<Context>().create();
+const { middleware, router } = t;
 
-    const { middleware, router } = t;
-
-    const userProcedure = t.procedure;
-
-    return {
-      middleware,
-      router,
-      userProcedure,
-    };
-  }
-};
-
-const { ...trpc } = trpcServer() || ({} as ReturnType<typeof trpcServer>);
-
-const { middleware, router, userProcedure } = trpc;
+const userProcedure = t.procedure;
 
 export { middleware, router, userProcedure };

3c50f7eef6ad523a90a57fc197354dd5f69007e4
kuubson <jxtoxic24@gmail.com>
2023-07-23 13:39:13 +0200

    feat: fix trpc type inference

--- apps/server/src/trpc/trpc.ts
+++ apps/server/src/trpc/trpc.ts
@@ -1,6 +1,6 @@
 import { initTRPC } from "@trpc/server";
 
-import type { Context } from "./api/router";
+import { type Context } from "./api/router";
 
 const t = initTRPC.context<Context>().create();
 

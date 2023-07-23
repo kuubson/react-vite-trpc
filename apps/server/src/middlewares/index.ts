cdae30c1e7f752cfd52e7293506014b6256d47e0
kuubson <jxtoxic24@gmail.com>
2023-07-23 13:43:25 +0200

    feat: fix trpc type inference

--- apps/server/src/middlewares/index.ts
+++ apps/server/src/middlewares/index.ts
@@ -1,10 +1,11 @@
 import cookieParser from "cookie-parser";
 import type { Application } from "express";
+import { RequestHandler } from "express-serve-static-core";
 
 import { initializeTrpc } from "trpc/api/router";
 
 export const middlewares = (app: Application) => {
-  app.use(cookieParser());
+  app.use(cookieParser() as RequestHandler);
 
   app.set("trust proxy", true);
 
---
+++ apps/server/src/types/index.ts
@@ -0,0 +1,1 @@
+import type {} from "express-serve-static-core";

509cd30d8a93277fa9d63e8579e1b6096712101d
kuubson <jxtoxic24@gmail.com>
2023-08-11 22:19:15 +0200

    feat: disable trust proxy (could bypass ratelimit)

--- apps/server/src/middlewares/index.ts
+++ apps/server/src/middlewares/index.ts
@@ -1,14 +1,11 @@
 import cookieParser from 'cookie-parser'
 import type { Application } from 'express'
 import { RequestHandler } from 'express-serve-static-core'
-
 import { initializeTrpc } from 'trpc/api/router'
 
 export class Middlewares {
    public static config(app: Application) {
       app.use(cookieParser() as RequestHandler)
-
-      app.set('trust proxy', true)
 
       initializeTrpc(app)
    }

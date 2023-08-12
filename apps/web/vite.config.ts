e2f8feab6239260c9f785c5bbc9f736fd8b3f244
kuubson <jxtoxic24@gmail.com>
2023-08-12 07:42:21 +0200

    feat: manually open the web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
       server: {
          host: true,
          port: 3000,
+         open: true,
          proxy: { '/trpc': { target: 'http://localhost:3001' } },
       },
    }
